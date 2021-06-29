30c8ce45414f39183de44b1af7568168f1235f4c
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-29 20:09:47 -0300

    Created variables

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -3,5 +3,13 @@ create a program that can encode and decode any
 message in another one with ceasar cipher method
 '''
 
+character_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
+'m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
+message = list(input('Type the message: ').lower())
+encrypt_key = int(input('Enter the key: '))
+process = input('Encode or decode: ').lower()
 
+print(message)
