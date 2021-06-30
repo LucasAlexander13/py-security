c14cf709b9108c1909f43b8ab8038e8944d8e00d
Lucas Alexander <lucasalexander13@hotmail.com>
2021-06-29 21:32:13 -0300

    Added replay

--- ceasar_cipher.py
+++ ceasar_cipher.py
@@ -6,36 +6,40 @@ message in another one with ceasar cipher method
 letter_list = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 
 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-message = list(input('Type the message: ').lower())
-encrypt_key = int(input('Enter the key: '))
-process = input('Encode or decode: ').lower()
-new_message = []
+while True:
+    message = list(input('Type the message: ').lower())
+    encrypt_key = int(input('Enter the key: '))
+    process = input('Encode or decode: ').lower()
+    new_message = []
 
-if process == 'encode':
-    for char in range(len(message)):
-        if message[char] == ' ':
-            new_message.append(' ')
-        elif message[char] in letter_list:
-            position = letter_list.index(message[char]) + encrypt_key
-            if position >= len(letter_list):
-                position = position - len(letter_list)
-                new_message.append(letter_list[position])
-            else:
-                new_message.append(letter_list[position])
+    if process == 'encode':
+        for char in range(len(message)):
+            if message[char] == ' ':
+                new_message.append(' ')
+            elif message[char] in letter_list:
+                position = letter_list.index(message[char]) + encrypt_key
+                if position >= len(letter_list):
+                    position = position - len(letter_list)
+                    new_message.append(letter_list[position])
+                else:
+                    new_message.append(letter_list[position])
 
-    new_message = ''.join(new_message)
-    print(f'Your message is: {new_message}')
+        new_message = ''.join(new_message)
+        print(f'\nYour message is: {new_message}\n')
 
-elif process == 'decode':
-    for char in range(len(message)):
-        if message[char] == ' ':
-            new_message.append(' ')
-        elif message[char] in letter_list:
-            position = letter_list.index(message[char]) - encrypt_key
-            if position < 0:
-                position = len(letter_list) + position
-                new_message.append(letter_list[position])
-            else:
-                new_message.append(letter_list[position])
-    new_message = ''.join(new_message)
-    print(f'Your message is: {new_message}')
+    elif process == 'decode':
+        for char in range(len(message)):
+            if message[char] == ' ':
+                new_message.append(' ')
+            elif message[char] in letter_list:
+                position = letter_list.index(message[char]) - encrypt_key
+                if position < 0:
+                    position = len(letter_list) + position
+                    new_message.append(letter_list[position])
+                else:
+                    new_message.append(letter_list[position])
+        new_message = ''.join(new_message)
+        print(f'\nYour message is: {new_message}\n')
+
+    else:
+        print(f'\nTry again, something goes wrong.\n')
